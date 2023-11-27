3793e567d474594a21ab82fde22d018b00909734
PauloHenriqueLopes <144734516+PauloHenriqueLopes@users.noreply.github.com>
2023-11-27 19:19:43 -0300

    Ajuste Pagina receitas_personalizadas

--- codigo/assets/js/receita.js
+++ codigo/assets/js/receita.js
@@ -1,28 +1,47 @@
-async function popularDiv(){
-
-    let dados = await buscarDados()
+async function popularDiv() {
+    let dados = await buscarDados();
     dados.forEach(dado => {
         var data = new Date();
-        var dia= data.getDate();
-        var mes=data.getMonth() + 1;
-        var ano=data.getFullYear();
-        let novaDiv = `<div id="teste" class:"d-fluid"><h4>${dado.nomerec}</h4><div class="row"><div class="col-4"><img src="https://source.unsplash.com/220x100/?healthfood" class="imagemDaReceita" data-receita-id=${dado.id}></div><div class="col-7" id:"corpo"> Nome : ${dado.nome} <br>Data:${dia}/${mes}/${ano}
-        <br>Descrição:${dado.descricao}</div></div></div>`
-        document.getElementById("perfis").innerHTML += novaDiv
-    })
+        var dia = data.getDate();
+        var mes = data.getMonth() + 1;
+        var ano = data.getFullYear();
+        let novaDiv = `<div id="teste" class="d-fluid">
+            <h4>${dado.nomerec}</h4>
+            <div class="row">
+                <div class="col-4">
+                    <img src="../assets/images/${dado.id}.jpg" class="imagemDaReceita" data-receita-id=${dado.id}>
+                </div>
+                <div class="col-7" id="corpo">
+                    Nome: ${dado.nome}<br>
+                    Data: ${dia}/${mes}/${ano}<br>
+                    Descrição: ${dado.descricao}
+                </div>
+            </div>
+        </div>`;
+        document.getElementById("perfis").innerHTML += novaDiv;
+    });
 }
 
 async function mostarModalDeIngredientes(id) {
-    let dados = await buscarDados()
+    let dados = await buscarDados();
     dados.forEach(dado => {
         if (dado.id == id) {
-            document.getElementById("ingredientesConteudo").innerHTML = `<div><p>${dado.ingredientes}</p><h3>Modo de preparo</h3><p>${dado.preparo}</p></div>`  ;
+            let ingredientesFormatados = dado.ingredientes.split(',').map(ingrediente => `<li>${ingrediente.trim()}</li>`).join('');
+            let preparoFormatado = dado.preparo.split(/\d+\-/).filter(item => item.trim() !== '').map(passo => `<li>${passo.trim()}</li>`).join('');
 
-            // Use a função modal do Bootstrap para mostrar o modal
+            document.getElementById("ingredientesConteudo").innerHTML = `
+                <div>
+                    <h3>Receita: ${dado.nomerec}</h3>
+                    <h4>Ingredientes</h4>
+                    <ul>${ingredientesFormatados}</ul>
+                    <h4>Modo de preparo</h4>
+                    <ol>${preparoFormatado}</ol>
+                </div>`;
+
             var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
             myModal.show();
         }
-    })
+    });
 }
 
 async function buscarDados(){
